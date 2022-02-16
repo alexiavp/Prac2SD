c22c4b12fa847a30138d7c25f9bfe57bb2947101
alexiavp <alexia.vidal.pons@gmail.com>
2022-02-16 11:32:06 +0100

    nosq

--- Prac1SD/main.py
+++ Prac1SD/main.py
@@ -1,1 +1,9 @@
-print ("hola")
+#!/usr/bin/env python3
+
+class Master:
+    __instance = None
+
+    def __new__(cls, *args):
+        if cls.__instance is None:
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
